25c89008ea26048b72a14efc88c2650a8de7918f
신희현 <hxxhyun@sinhuihyeon-ui-MacBookPro.local>
2024-09-24 21:22:41 +0900

    캐로우셀

---
+++ app.js
@@ -0,0 +1,21 @@
+alert("Hello, World");
+
+const rightArrow = document.querySelector(".right");
+const leftArrow = document.querySelector(".left");
+const lists = document.querySelectorAll("li");
+
+let curIndex = 0;
+
+rightArrow.onclick = function () {
+  lists[curIndex].classList.remove("show");
+  curIndex++;
+  if (curIndex === lists.length) curIndex = 0;
+  lists[curIndex + 1].classList.add("show");
+};
+
+leftArrow.onclick = function () {
+  lists[curIndex].classList.remove("show");
+  curIndex--;
+  if (curIndex === -1) curIndex = 4;
+  lists[curIndex - 1].classList.add("show");
+};
